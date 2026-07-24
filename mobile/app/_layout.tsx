8fce911acea5adc70ef56edf66fae4ce0c9bb48c
saiklonee <saiprithvitech@gmail.com>
2026-07-24 20:36:39 +0530

    add nativewind

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,12 @@
-import { Stack } from "expo-router";
+import "@/global.css";
+import { Text, View } from "react-native";
 
-export default function RootLayout() {
-  return <Stack />;
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
 }
